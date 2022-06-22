07155d3833c244311032d54ed2b30385be6fc3ee
Vishalhumber <n01496099@humbermail.ca>
2022-06-21 23:35:30 -0400

    update php files

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # projects
+
 # PersonalProjectJune
+
 # DiceSimulator
+
 # Quiz_Game_Starter
-# deadline : 23/06/2022
--- index.php
+++ index.php
@@ -1,8 +1,14 @@
-<html>
- <head>
-  <title>PHP Test First Php hands on</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- </body>
+<!DOCTYPE html >
+    <html lang = "en">
+    <head>
+    <meta charset = " UTF - 8 " >
+    <meta name = " viewport " content = " width = device - width , initial - scale = 1.0 " >
+    <head title>PHP Tutorial</title>
+    </head>
+    <body>
+    <div class = "container">
+    This is my first php website
+    <?php echo "Vishal Hello World this is printed using PHP"; // Search Algo ?>
+    </div>
+    </body>
 </html>
